da2269061c1ef9ccc19979986d739199e8455545
fangliu520 <516841480@qq.com>
2021-04-03 23:26:56 +0800

    apollo配置项目

---
+++ webnet5.apollo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using webnet5.apollo.Models;
+
+namespace webnet5.apollo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _iConfiguration = null;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _iConfiguration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ConfigA = this._iConfiguration["config_a"];
+            ViewBag.ConfigB = this._iConfiguration["config_b"];
+            ViewBag.ConfigC = this._iConfiguration["config_c"];
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ webnet5.apollo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webnet5.apollo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
